e8cee8c4927eedca0772a296a66c7a4d846f65aa
Alexis <alexis@gmail.com>
2026-04-02 17:35:33 +0200

    plantilla base creada

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+ function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-neutral-950 text-white">
+      <h1 className="text-4xl font-bold">Plantilla lista</h1>
+    </div>
+  )
+}
+
+
+export default App;
